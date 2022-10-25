1ae5a209e4ea14927e34a6cd17eb85754c507da4
Z-kamili <zakariakamili97@gmail.com>
2022-10-25 12:53:32 +0100

    build three widget

--- lib/main.dart
+++ lib/main.dart
@@ -11,12 +11,30 @@ class MyApp extends StatelessWidget {
 
   Widget build(BuildContext context) {
 
+        var question = [
+          'What\'s your favorite color?',
+          'What\'s your favorite animal?',
+        ];
+
         return MaterialApp(home:Scaffold(
            
-           appBar: AppBar(title: Text('My First App'),),
-           body: Text('This is my default text!'),
+           appBar: AppBar(title: Text('My First App'),),//AppBar
+           body: Column(children:[
+              Text('The question!'),
+              ElevatedButton(
+                child:Text('Answer 1'),
+                onPressed:null),
+              ElevatedButton(
+                child:Text('Answer 2'),
+                onPressed:null),
+              ElevatedButton(
+                child:Text('Answer 3'),
+                onPressed:null),
+           ],),
+           //Column
 
         ),
+
         );
 
    }
