c400bcc2ceb2ba09183902606195b6c855cf2fb0
Z-kamili <zakariakamili97@gmail.com>
2022-10-25 12:06:19 +0100

    start creating a three widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,23 @@
 import 'package:flutter/material.dart';
-void main() {
+// void main() {
 
- runApp(MyApp());
+//  runApp(MyApp());
    
-}
+// }
+
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
 
   Widget build(BuildContext context) {
 
-        return MaterialApp(home:Text('Hello!'));
+        return MaterialApp(home:Scaffold(
+           
+           appBar: AppBar(title: Text('My First App'),),
+           body: Text('This is my default text!'),
+
+        ),
+        );
 
    }
 
