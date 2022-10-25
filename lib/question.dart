6aac6a9131401e055727d8ebae8bb2951f8dfb63
Z-kamili <zakariakamili97@gmail.com>
2022-10-25 18:36:07 +0100

    more style

--- lib/question.dart
+++ lib/question.dart
@@ -13,6 +13,7 @@ Widget build(BuildContext context) {
    return 
    Container(
     width: double.infinity,
+    margin: EdgeInsets.all(10),
     child : Text(
     questionText,
     style:TextStyle(fontSize: 28),
