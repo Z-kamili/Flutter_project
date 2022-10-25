aa6d8f680f6c5e91edf67c4cf4f643c12495f08e
Z-kamili <zakariakamili97@gmail.com>
2022-10-25 13:12:21 +0100

    problem fixed

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,7 @@
+// ignore: duplicate_ignore
+// ignore: duplicate_ignore
+// ignore_for_file: prefer_const_constructors, duplicate_ignore
+
 import 'package:flutter/material.dart';
 // void main() {
 
@@ -5,31 +9,49 @@ import 'package:flutter/material.dart';
    
 // }
 
+// ignore: prefer_const_constructors
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
+
+  void answerQuestion()
+  {
+    // ignore: avoid_print
+    print('Answer chosen!');
+  }
+
+  @override
   Widget build(BuildContext context) {
 
+        // ignore: unused_local_variable
         var question = [
           'What\'s your favorite color?',
           'What\'s your favorite animal?',
         ];
 
+        // ignore: duplicate_ignore, duplicate_ignore
         return MaterialApp(home:Scaffold(
            
+           // ignore: prefer_const_constructors
            appBar: AppBar(title: Text('My First App'),),//AppBar
+           // ignore: duplicate_ignore
            body: Column(children:[
+              // ignore: prefer_const_constructors
               Text('The question!'),
               ElevatedButton(
-                child:Text('Answer 1'),
-                onPressed:null),
+                // ignore: sort_child_properties_last
+                child: Text('Answer 1'),
+                onPressed:answerQuestion),
               ElevatedButton(
+                // ignore: sort_child_properties_last
                 child:Text('Answer 2'),
-                onPressed:null),
+                onPressed:answerQuestion),
               ElevatedButton(
+                // ignore: sort_child_properties_last
                 child:Text('Answer 3'),
-                onPressed:null),
+                onPressed:answerQuestion),
            ],),
            //Column
 
